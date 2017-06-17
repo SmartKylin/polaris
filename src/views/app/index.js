ee92740920702d1f24e8abbbf0f66978cde47284
xiaoyan <xiao.yan@jinyinwu.com>
2017-06-17 21:08:09 +0800

    子页面隐藏 TabBar

--- src/views/app/index.js
+++ src/views/app/index.js
@@ -1,29 +1,20 @@
 import create from './index.tpl'
 import './index.styl'
 
+const rootPaths = [
+  '/tentacle',
+  '/clue',
+  '/user'
+]
+
 export default create({
-  data() {
-    return {
-      visible: true,
-    }
-  },
-  mounted () {
-    this.state = window.location.hash.slice(2);
-    window.addEventListener('hashchange', () => {
-      this.state = window.location.hash.slice(2);
-      if (this.state == "clue" || this.state == "tentacle" || this.state == "user") {
-        this.visible = true;
-      } else {
-        this.visible = false;
-      }
-    }, false);
-  },
   computed: {
     route() {
       return this.$route.path;
+    },
+    visible() {
+      return rootPaths.indexOf(this.$route.path) > -1
     }
-  },
-  methods: {
   }
 })
 
