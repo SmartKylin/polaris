69142a7ef663b0c58546ea5fbbac8e6dadc078a2
xiaoyan <xiao.yan@jinyinwu.com>
2017-06-23 10:15:46 +0800

    创建 datepicker && 在 clue/detail 中使用

---
+++ src/helper/datepicker.js
@@ -0,0 +1,59 @@
+import Picker from 'better-picker'
+
+const years = []
+const months = []
+const days = []
+const hours = []
+const minutes = []
+const seconds = []
+
+;(function() {
+  for (let i = 2017; i < 2039; i++) {
+    // 年
+    years.push({ text: i + '年', value: i })
+  }
+
+  for (let i = 1; i < 61; i++) {
+    // 月
+    if (i < 13) {
+      months.push({ text: i + '月', value: i })
+    }
+    // 日
+    if (i < 32) {
+      days.push({ text: i + '日', value: i })
+    }
+    // 时
+    if (i < 25) {
+      hours.push({ text: i + '时', value: i })
+    }
+    // 分
+    if (i < 61) {
+      minutes.push({ text: i + '分', value: i })
+      // 秒
+      seconds.push({ text: i + '秒', value: i })
+    }
+  }
+})()
+
+const picker = new Picker({
+  data: [ years, months, days, hours, minutes ],
+  title: '日期时间选择'
+})
+
+export default {
+  show() {
+    picker.show()
+  },
+
+  onselect(callback) {
+    picker.on('picker.select', values => {
+      values = values.map(v => {
+        if (v < 10) {
+          v = '0' + v
+        }
+        return v
+      })
+      callback(`${values[0]}-${values[1]}-${values[2]} ${values[3]}:${values[4]}`)
+    })
+  }
+}
