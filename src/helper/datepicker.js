98861181cdcaecd618b0f6ddf57c9471ba08a975
gayechen <gayechen@gayechendeMacBook-Pro.local>
2017-06-27 21:12:53 +0800

    datepick控件默认时间为当前时间

--- src/helper/datepicker.js
+++ src/helper/datepicker.js
@@ -7,48 +7,61 @@ const hours = []
 const minutes = []
 const seconds = []
 
-// const now = new Date()
+const yearAry = []
+const monthAry = []
+const dayAry = []
+const hoursAry = []
+const minuteAry = []
+const secondAry = []
 
-/*const defaultIndex = [
-  years.indexOf(now.getFullYear()),
-  months.indexOf(now.getMonth() + 1),
-  days.indexOf(now.getDate()),
-  hours.indexOf(now.getHours()),
-  minutes.indexOf(now.getMinutes()),
-  seconds.indexOf(now.getSeconds())
-  ]*/
 
-;(function() {
-  for (let i = 2017; i < 2039; i++) {
+;(function () {
+  for (let i =  2017; i < 2039; i++) {
     // 年
-    years.push({ text: i + '年', value: i })
+    years.push({text: i + '年', value: i})
+    yearAry.push(i)
   }
 
   for (let i = 1; i < 61; i++) {
     // 月
     if (i < 13) {
-      months.push({ text: i + '月', value: i })
+      months.push({text: i + '月', value: i})
+      monthAry.push(i)
     }
     // 日
     if (i < 32) {
-      days.push({ text: i + '日', value: i })
+      days.push({text: i + '日', value: i})
+      dayAry.push(i)
     }
     // 时
     if (i < 25) {
-      hours.push({ text: i + '时', value: i })
+      hours.push({text: i + '时', value: i})
+      hoursAry.push(i)
     }
     // 分
     if (i < 61) {
-      minutes.push({ text: i + '分', value: i })
+      minutes.push({text: i + '分', value: i})
       // 秒
-      seconds.push({ text: i + '秒', value: i })
+      seconds.push({text: i + '秒', value: i})
+      minuteAry.push(i)
+      secondAry.push(i)
     }
   }
 })()
 
+const now  = new Date()
+const defaultIndex = [
+  yearAry.indexOf(now.getFullYear()),
+  monthAry.indexOf(now.getMonth() + 1),
+  dayAry.indexOf(now.getDate()),
+  hoursAry.indexOf(now.getHours()),
+  minuteAry.indexOf(now.getMinutes()),
+  secondAry.indexOf(now.getSeconds())
+]
+
 const picker = new Picker({
-  data: [ years, months, days, hours, minutes ],
-  // selectedIndex: [1, 3, 4, 5, 6]
+  data: [years, months, days, hours, minutes],
+  selectedIndex: defaultIndex,
   title: '日期时间选择'
 })
 
