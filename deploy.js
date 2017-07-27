0230a52335747f94e0939c419f2be43167d00f29
xiaoyan <xiao.yan@jinyinwu.com>
2017-07-27 22:55:05 +0800

    修改 sftp 账号配置

--- deploy.js
+++ deploy.js
@@ -1,5 +1,5 @@
 module.exports = {
-  server: 'xiaofeng:xiaofeng@10.13.3.4',
+  server: 'root:bbt@10.5.2.1:5004',
   workspace: __dirname + '/dist',
   ignore: '**/*.map',
   deployTo: '/data1/htdocs/mizar',
